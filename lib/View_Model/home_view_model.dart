b5da08de64c53b2b66e37f91c2c71537639c585a
GirikratnaSharma <girikratnasharma@gmail.com>
2022-05-24 18:26:51 +0530

    Final Commit

---
+++ lib/View_Model/home_view_model.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:tdd_module/base/base_model.dart';
+
+class HomeViewModel extends BaseModel {}
---
+++ lib/base/base_model.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:tdd_module/utils/view_state.dart';
+
+class BaseModel with ChangeNotifier {
+  ViewState _state = ViewState.Idle;
+
+  ViewState get state => _state;
+
+  set state(ViewState viewState) {
+    print('State:$viewState');
+    _state = viewState;
+    notifyListeners();
+  }
+
+  void update() {
+    notifyListeners();
+  }
+}
